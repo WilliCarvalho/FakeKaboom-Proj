211acf2c28fe02cb62cf0978cfc9441a57ca8279
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2024-05-10 20:00:19 -0300

    te amo

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerBehavior : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private int scoreToDecrease;
 
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         float inputMoveX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+        if(inputMoveX != 0)
+        {
+            //Está se movendo
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            //Está parado
+            animator.SetBool("isMoving", false);
+        }
         transform.Translate(inputMoveX, 0, 0);
     }
 
